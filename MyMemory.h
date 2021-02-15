a3a5679a198202afa709cb16fa22239fefbc6887
AldricVS <aldric.silvestre@outlook.fr>
2021-02-15 15:53:27 +0100

    Ajout d'un AUTRE COMMENTAIRE (FUNZZZ)

--- MyMemory.h
+++ MyMemory.h
@@ -1,6 +1,9 @@
 #ifndef MY_MEMORY_H
 #define MY_MEMORY_H
 
+/**
+* MyMemory contains an array is where myAlloc and myFree will "work"
+*/
 typedef struct mymemory{
 	unsigned int size;
 	int isFree;
