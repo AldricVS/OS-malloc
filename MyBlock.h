3bced7fc37b46716646b7d7494230cf15d720195
AldricVS <aldric.silvestre@outlook.fr>
2021-02-15 15:31:05 +0100

    Ajout d'un commentaire pour tester git

--- MyBlock.h
+++ MyBlock.h
@@ -1,6 +1,11 @@
 #ifndef MY_BLOCK_H
 #define MY_BLOCK_H
 
+/**
+ * A block is an entity that will be in the memory. 
+ * His purpose is to hold data from allocated blocks and permit 
+ * to keep trace of them (when allocationg a new memory zone or freeing it)
+ */
 typedef struct myBlockCel {
 	unsigned int contentSize;
 	void *contentPtr;
